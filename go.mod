1716b211b917562a1ba237cacf07d0930e04640f
liyanjiang <liyanjiang@sohu-inc.com>
2021-06-16 18:14:41 +0800

    // modify

---
+++ go.mod
@@ -0,0 +1,8 @@
+module algorithm
+
+go 1.15
+
+require (
+	github.com/lyjgulu/algorithm v0.0.0-20210616093044-4f47416eeca6
+	github.com/stretchr/testify v1.7.0
+)
---
+++ leetcode/lc0094/lc0094.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import "github.com/lyjgulu/algorithm/structures"
+
+// TreeNode define
+type TreeNode = structures.TreeNode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// 递归
+func inorderTraversal(root *TreeNode) []int {
+	var result []int
+	inorder(root, &result)
+	return result
+}
+
+func inorder(root *TreeNode, output *[]int) {
+	if root != nil {
+		inorder(root.Left, output)
+		*output = append(*output, root.Val)
+		inorder(root.Right, output)
+	}
+}
