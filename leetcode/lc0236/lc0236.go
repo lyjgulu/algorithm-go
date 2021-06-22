c2615b3e80b054f416ae5ce63675cbef997c0d44
liyanjiang <liyanjiang@sohu-inc.com>
2021-06-22 14:44:10 +0800

    // modify

---
+++ leetcode/lc0236/lc0236.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import "github.com/lyjgulu/algorithm/structures"
+
+// TreeNode define
+type TreeNode = structures.TreeNode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+	if left == nil {
+		return right
+	}
+	return left
+}
---
+++ leetcode/lc0236/lc0236_test.go
@@ -0,0 +1,46 @@
+package leetcode
+
+import (
+	"fmt"
+	"github.com/lyjgulu/algorithm/structures"
+	"testing"
+)
+
+type question236 struct {
+	para236
+	ans236
+}
+
+// para 是参数
+// one 代表第一个参数
+type para236 struct {
+	one []int
+}
+
+// ans 是答案
+// one 代表第一个答案
+type ans236 struct {
+	one []int
+}
+
+// TODO
+func Test_Problem236(t *testing.T) {
+
+	qs := []question236{
+
+		{
+			para236{[]int{4, 2, 7, 1, 3, 6, 9}},
+			ans236{[]int{4, 7, 2, 9, 6, 3, 1}},
+		},
+	}
+
+	fmt.Printf("------------------------Leetcode Problem 236------------------------\n")
+
+	for _, q := range qs {
+		_, p := q.ans236, q.para236
+		fmt.Printf("【input】:%v      ", p)
+		root := structures.Ints2TreeNode(p.one)
+		fmt.Printf("【output】:%v      \n", structures.Tree2ints(invertTree(root)))
+	}
+	fmt.Printf("\n\n\n")
+}
